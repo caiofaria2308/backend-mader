f5b46c1059aadf36c1f3b95d513954ff97427a9f
Caio de Faria <caiofaria2308@gmail.com>
2021-11-23 14:54:10 -0300

    init app product

---
+++ product/views.py
@@ -0,0 +1,4 @@
+from defaults import DefaultMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework import filters
